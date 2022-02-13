5cea4e6ef79e17732b761e3e7cd471cb86b57c67
lukereynoldspi <lukereynoldspi@gmail.com>
2022-02-13 02:09:24 -0800

    made head global variable

--- myalloc.c
+++ myalloc.c
@@ -1,10 +1,13 @@
+// Luke Reynolds
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
 #define ALIGNMENT 16   // Must be power of 2
-#define GET_PAD(x) ((ALIGNMENT - 1) - ((x) - 1) & (ALIGNMENT - 1))
+#define GET_PAD(x) ((ALIGNMENT - 1) - (((x) - 1) & (ALIGNMENT - 1)))
+#define PADDED_SIZE(x) ((x) + GET_PAD(x))
 
 struct block {
     struct block *next;
@@ -12,27 +15,38 @@ struct block {
     int in_use;   // Boolean
 }
 
-int main(int argc, char *argv[]) {
-	void *heap = sbrk(1024);
-	struct block *head = NULL;
+struct block *head = NULL;
+
+int main(void) {
+    void *p;
+    print_data();
+	
 
 }
 
-void myalloc(){
-	
+void myalloc(int bytes){
+
+	void *heap = sbrk(1024);
+
 	if (head == NULL) {
     head = sbrk(1024);
     head->next = NULL;
     head->size = 1024 - PADDED_SIZE(sizeof(struct block));
     head->in_use = 0;
+    }
+
+    int actual_size = PADDED_SIZE(bytes);
 
     struct block *cur;
+    cur->next = NULL;
+    cur->size = actual_size;
+    cur->in_use = 1;
 
-	// ... All the machinations to allocate go here ...
 
-	padded_block_size = PADDED_SIZE(sizeof(struct block));
+	int padded_block_size = PADDED_SIZE(sizeof(struct block));
 
-	return PTR_OFFSET(cur, padded_block_size);
+    
+    return PTR_OFFSET(cur, padded_block_size);
 }
 
 void print_data(void)
@@ -46,7 +60,7 @@ void print_data(void)
 
     while (b != NULL) {
         // Uncomment the following line if you want to see the pointer values
-        //printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
         printf("[%d,%s]", b->size, b->in_use? "used": "free");
         if (b->next != NULL) {
             printf(" -> ");
@@ -56,5 +70,4 @@ void print_data(void)
     }
 
     printf("\n");
-}
 }
