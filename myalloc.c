99e5ff0d218a81953f68cbde8e749ea7770af812
Luke Reynolds <lukereynoldspi@gmail.com>
2022-02-20 03:16:51 -0800

    finished some stuff from last week, somehow missed an include statement?????????

--- myalloc.c
+++ myalloc.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define ALIGNMENT 16   // Must be power of 2
 #define GET_PAD(x) ((ALIGNMENT - 1) - (((x) - 1) & (ALIGNMENT - 1)))
@@ -18,47 +19,41 @@ struct block {
     int in_use;   // Boolean
 };
 
-int main(void) {
-
-    void *p;
-
-    print_data();
-    p = myalloc(64);
-    print_data();
-
-}
-
 void * myalloc(int bytes){
 
 	if (head == NULL) {
     head = sbrk(1024);
     head->next = NULL;
-    head->size = 1024 - PADDED_SIZE(sizeof(struct block));
+    head->size = 1024 - PADDED_SIZE(sizeof(struct block)); // 1008 bytes
     head->in_use = 0;
     }
+    int actual_size = PADDED_SIZE(bytes); // mult of 16
+    int padded_block_size = PADDED_SIZE(sizeof(struct block)); // 16 bytes
 
-    else {
-        int actual_size = PADDED_SIZE(bytes);
+    //struct block *cur = head->next;
+    //head->in_use = 1;
+    while (cur) {
+        if (cur->in_use == 0 && (actual_size + padded_block_size <= cur->size)) {
 
-        struct block *cur = head->next;
-        
-        while (cur) {
-            if (cur->in_use == 0 && (actual_size + PADDED_SIZE(sizeof(struct block))) < cur->size) {
-                cur->size = actual_size;
-                cur->in_use = 1;
+            cur->size = actual_size;
+            cur->in_use = 1;
 
-                int padded_block_size = PADDED_SIZE(sizeof(struct block));
+            struct block *nxt = PTR_OFFSET(cur, padded_block_size + actual_size); // struct block to separate memory pools
+            nxt->next = NULL;
+            nxt->in_use = 0;
+            nxt->size = cur->size - (actual_size + padded_block_size);
 
-                return PTR_OFFSET(cur, padded_block_size);
-                }
-            
-            else {
-                cur = cur->next;
+            cur->next = nxt;
+
+            return PTR_OFFSET(cur, padded_block_size);
             }
+            
+        else {
+            cur = cur->next;
         }
+    }
         
     return NULL;
-    }
 }
 
 void print_data(void)
@@ -72,7 +67,7 @@ void print_data(void)
 
     while (b != NULL) {
         // Uncomment the following line if you want to see the pointer values
-        printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        //printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
         printf("[%d,%s]", b->size, b->in_use? "used": "free");
         if (b->next != NULL) {
             printf(" -> ");
@@ -82,4 +77,21 @@ void print_data(void)
     }
 
     printf("\n");
+}
+
+int main(void) {
+
+    void *p;
+
+    print_data();
+    p = myalloc(16);
+    print_data();
+    p = myalloc(16);
+    print_data();
+    p = myalloc(16);
+    print_data();
+    p = myalloc(2000);
+    print_data();
+    printf("%p\n", p);
+
 }
